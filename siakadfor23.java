121f46d2d90a700086c7c749b1973d026a5f3170
radit <pradityapramana21@gmail.com>
2025-11-07 10:25:34 +0700

    modivikasi 1

--- siakadfor23.java
+++ siakadfor23.java
@@ -1,19 +1,32 @@
 import java.util.Scanner;
+
 public class siakadfor23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
-        for (int i = 1; i <= 10; i++){
-            System.out.println(" Masukan nilai mahasiswa ke-" + i + ":");
+        int lulus = 0, tidakLulus = 0;
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
+
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
             if (nilai < terendah) {
                 terendah = nilai;
             }
-        System.out.println("Nilai tertinggi:" + tertinggi);
-        System.out.println("Nilai terendah:" + terendah);
+
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
+        System.out.println("\n=== HASIL AKHIR ===");
+        System.out.println("Nilai tertinggi : " + tertinggi);
+        System.out.println("Nilai terendah  : " + terendah);
+        System.out.println("Jumlah mahasiswa lulus     : " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus : " + tidakLulus);
     }
 }
